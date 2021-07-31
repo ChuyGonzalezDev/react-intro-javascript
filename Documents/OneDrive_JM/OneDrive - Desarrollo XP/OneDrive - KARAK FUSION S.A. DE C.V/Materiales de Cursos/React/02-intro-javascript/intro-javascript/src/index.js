1e448d80fde3caf6690add6b79010d0004a6619c
Jesús González <chuygonzalez.dev@gmail.com>
2021-07-31 11:34:11 -0500

    Commit inicial de ejercicio

--- Documents/OneDrive_JM/OneDrive - Desarrollo XP/OneDrive - KARAK FUSION S.A. DE C.V/Materiales de Cursos/React/02-intro-javascript/intro-javascript/src/index.js
+++
@@ -1,6 +0,0 @@
-const apiKey = "6F5TkPRcFw8TFoto82GGSf3W2yNExEZ6";
-
-const petition = fetch(`https://api.giphy.com/v1/gifs/random?apiKey=${apiKey}`);
-petition.then ( resp => {
-    console.log(resp);
-});
